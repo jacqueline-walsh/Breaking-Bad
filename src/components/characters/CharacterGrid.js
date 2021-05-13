f89a6d35a1c2f67c6f3132d5b45c33cb2275c6d3
Jacqueline-Walsh <jacqueline.walsh34@gmail.com>
2021-05-13 10:00:02 +0100

    Initial upload

---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,13 @@
+import CharacterItem from './CharacterItem'
+import Spinner from '../ui/Spinner'
+
+
+const CharacterGrid = ({items, isLoading}) => {
+    return isLoading ? (<Spinner></Spinner>) : (<section className="cards">
+            {items.map(item => [
+                <CharacterItem key={item.char_id} item={item}></CharacterItem>
+            ])}
+        </section>)
+}
+
+export default CharacterGrid
---
+++ src/components/ui/Header.js
@@ -0,0 +1,11 @@
+import logo from '../../img/logo.png'
+
+const Header = () => {
+    return (
+        <header className='center'>
+            <img src={logo} alt="logo for breaking bad cast"/>
+        </header>
+    )
+}
+
+export default Header
